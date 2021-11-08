e7205bbd425d8ec0e126b70eb71c3d5621322c02
Hasibul294 <hasibulhasan294@gmail.com>
2021-11-09 01:57:39 +0600

    date fix

--- index.js
+++ index.js
@@ -43,9 +43,7 @@ async function run() {
 
     app.get("/appointments", verifyToken, async (req, res) => {
       const email = req.query.email;
-      // const date = req.query.date;
-      const date = new Date(req.query.date).toLocaleDateString();
-      //   console.log(date);
+      const date = req.query.date;
       const query = { email: email, date: date };
       const cursor = appointmentCollection.find(query);
       const appointments = await cursor.toArray();
